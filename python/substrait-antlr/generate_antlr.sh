7b8cf64ce04c5ead863b22d14940d8821e3220c6
Victor Barua <victor.barua@datadoghq.com>
2026-02-26 16:16:20 -0800

    ci(python): scaffolding for substrait-antlr (#5)

---
+++ python/substrait-antlr/generate_antlr.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+set -eu
+
+SUBSTRAIT_HOME="${SUBSTRAIT_HOME:-../../substrait}"
+GRAMMAR_DIR="$SUBSTRAIT_HOME/grammar"
+
+TYPES_TARGET="src/substrait_antlr/substrait_type"
+FTEST_TARGET="src/substrait_antlr/func_test_case"
+
+echo "Generating Python ANTLR files from $GRAMMAR_DIR"
+
+# Copy grammars to tmp directory
+rm -rf tmp
+mkdir -p tmp
+cp -r "$GRAMMAR_DIR"/*.g4 tmp
+
+# Generate ANTLR parsers in tmp directory
+cd tmp
+antlr4 -Dlanguage=Python3 -visitor SubstraitLexer.g4 SubstraitType.g4
+antlr4 -Dlanguage=Python3 -visitor FuncTestCaseLexer.g4 FuncTestCaseParser.g4
+cd ..
+
+# Clean target directories
+rm -rf $TYPES_TARGET
+mkdir -p $TYPES_TARGET
+
+rm -rf $FTEST_TARGET
+mkdir -p $FTEST_TARGET
+
+# Copy generated Python files to target directories
+cp tmp/Substrait*.py $TYPES_TARGET
+cp tmp/FuncTestCase*.py $FTEST_TARGET
+
+# Cleanup
+rm -rf tmp
---
+++ python/substrait-antlr/tests/test_antlr.py
@@ -0,0 +1,49 @@
+from antlr4 import InputStream, CommonTokenStream
+from pathlib import Path
+
+from substrait_antlr.substrait_type.SubstraitTypeLexer import SubstraitTypeLexer
+from substrait_antlr.substrait_type.SubstraitTypeParser import SubstraitTypeParser
+from substrait_antlr.substrait_type.SubstraitTypeVisitor import SubstraitTypeVisitor
+from substrait_antlr.func_test_case.FuncTestCaseLexer import FuncTestCaseLexer
+from substrait_antlr.func_test_case.FuncTestCaseParser import FuncTestCaseParser
+from substrait_antlr.func_test_case.FuncTestCaseParserVisitor import FuncTestCaseParserVisitor
+
+# Make sure that SubstraitTypeVisitor is available
+class SubstraitDummyVisitor(SubstraitTypeVisitor):
+    pass
+
+# Make sure that FuncTestCaseParserVisitor is available
+class FuncTestDummyVisitor(FuncTestCaseParserVisitor):
+    pass
+
+def test_type_parser_simple_type():
+    lexer = SubstraitTypeLexer(InputStream("i32"))
+    parser = SubstraitTypeParser(CommonTokenStream(lexer))
+
+    assert parser.typeDef().scalarType().toStringTree(recog=parser) == '(scalarType i32)'
+
+def test_type_parser_composite_type():
+    lexer = SubstraitTypeLexer(InputStream("list?<any1>>"))
+    parser = SubstraitTypeParser(CommonTokenStream(lexer))
+
+    assert parser.typeDef().parameterizedType().toStringTree(recog=parser) == '(parameterizedType list ? < (expr (typeDef (anyType any1))) >)'
+
+def test_scalar_test_parsing():
+    tc = load_test_case("test_scalar_test.yaml")
+    lexer = FuncTestCaseLexer(InputStream(tc))
+    parser = FuncTestCaseParser(CommonTokenStream(lexer))
+
+    assert parser.doc().header().version().toStringTree(recog=parser) == '(version ### SUBSTRAIT_SCALAR_TEST : v1.0)'
+
+def test_aggregate_test_parsing():
+    tc = load_test_case("test_aggregate_test.yaml")
+    lexer = FuncTestCaseLexer(InputStream(tc))
+    parser = FuncTestCaseParser(CommonTokenStream(lexer))
+
+    assert parser.doc().header().version().toStringTree(recog=parser) == '(version ### SUBSTRAIT_AGGREGATE_TEST : v1.0)'
+
+def load_test_case(testname):
+    """Load and return the content of antlr/test/<file>"""
+    test_file = Path(__file__).parent.parent.parent.parent / "antlr" / "tests" / testname
+    with open(test_file, "r") as f:
+        return f.read()
